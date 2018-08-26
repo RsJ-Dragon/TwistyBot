0f66119d6675f45646c097026d30e9110ea9da4b
LinearSpoon <linearality@gmail.com>
2018-08-26 14:25:46 -0400

    Change invite link to #public

--- src/commands/General/invite.js
+++ src/commands/General/invite.js
@@ -11,6 +11,6 @@ module.exports.aliases = [ 'invite_link' ];
 
 module.exports.run = async function(Discord, client, params, options) {
 	return "TwistyBot: " + Discord.link('https://discordapp.com/oauth2/authorize?client_id=228019028755611648&permissions=0&scope=bot') +
-		"\nRuneWatch: " + Discord.link('https://discord.gg/3yCq35d');
+		"\nRuneWatch: " + Discord.link('https://discord.gg/umeNzN7');
 	// return Discord.link(`https://discordapp.com/oauth2/authorize?client_id=${ client.user.id }&permissions=0&scope=bot`);
 };
